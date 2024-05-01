a4322aea654f4c5a81bd05819faa1e834955726b
Arxeno <rmfbkw04@gmail.com>
2024-05-02 00:36:14 +0700

    generate all module

--- authentication/views.py
+++ authentication/views.py
@@ -1,1 +1,8 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+
+class LoginView(APIView):
+    def post(self, request: Request) -> Response:
+        ...
