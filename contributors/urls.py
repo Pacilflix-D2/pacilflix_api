2141134001ebd653bcad7ed9a08bd88cf033ee2d
Arxeno <rmfbkw04@gmail.com>
2024-05-02 10:28:25 +0700

    create base api for contributors

--- contributors/urls.py
+++ contributors/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
 
-urlpatterns = []
+from contributors.views import AllContributorsView, AllPemainView, AllScriptWriterView, AllSutradaraView
+
+urlpatterns = [
+    path('', AllContributorsView.as_view()),
+    path('sutradara/', AllSutradaraView.as_view()),
+    path('pemain/', AllPemainView.as_view()),
+    path('penulis-skenario/', AllScriptWriterView.as_view())
+]
--- contributors/views.py
+++ contributors/views.py
@@ -1,1 +1,23 @@
 from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+class AllContributorsView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
+
+
+class AllSutradaraView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
+
+
+class AllPemainView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
+
+
+class AllScriptWriterView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
