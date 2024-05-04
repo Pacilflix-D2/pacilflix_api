a90e63a004e723e41f904ce31a194554619d8c37
Arxeno <rmfbkw04@gmail.com>
2024-05-04 19:17:26 +0700

    feat: get all film, get film details

--- core/repositories/film.py
+++ core/repositories/film.py
@@ -1,10 +1,55 @@
 from core.models.film import Film
 from core.repositories.database import Database
+from core.utils.exceptions.not_found import NotFoundException
 
 
 class FilmRepository(Database):
     def find_all(self) -> list[Film]:
-        ...
+        try:
+            film_tuples = self.query('SELECT * FROM film')
+        except:
+            raise NotFoundException('Cannot find films.')
+        # print('WOW')
+        # print(film_tuples)
 
-    def find_by_id(self, id: str) -> Film:
-        ...
+        result: list[Film] = []
+        for film in film_tuples:
+            # print('HERE')
+            # print(film[3])
+            # print(type(film[3]))
+
+            result.append(
+                Film(
+                    id_tayangan=str(film[0]),
+                    url_video_film=film[1],
+                    release_date_film=film[2],
+                    durasi_film=film[3],
+                )
+            )
+
+        return result
+
+    def find_by_id(self, id_tayangan: str) -> Film:
+        try:
+            film_tuples = self.query(
+                f"SELECT * FROM film WHERE id_tayangan = '{id_tayangan}'")
+        except:
+            raise NotFoundException('Cannot find film.')
+
+        print('LEN')
+        print(len(film_tuples))
+
+        if (len(film_tuples) == 0):
+            print('throwed')
+            raise NotFoundException('Cannot found film.')
+
+        film_tuple = film_tuples[0]
+
+        result: Film = Film(
+            id_tayangan=str(film_tuple[0]),
+            url_video_film=film_tuple[1],
+            release_date_film=film_tuple[2],
+            durasi_film=film_tuple[3],
+        )
+
+        return result
--- film/views.py
+++ film/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from core.models.film import Film
 from core.repositories.film import FilmRepository
+from core.utils.exceptions.not_found import NotFoundException
 from core.utils.response import Response
 from rest_framework import status
 
@@ -10,8 +11,11 @@ from rest_framework import status
 class FilmListView(APIView):
     def get(self, request: Request) -> Response:
         film_repository = FilmRepository()
+        # print('hi')
 
         films: list[Film] = film_repository.find_all()
+        # print('FILMS')
+        # print(films)
 
         data_json: list[dict[str, Any]] = []
         for film in films:
@@ -22,9 +26,18 @@ class FilmListView(APIView):
 
 class FilmDetailView(APIView):
     def get(self, request: Request, id_tayangan: str) -> Response:
-        ...
+        film_repository = FilmRepository()
+
+        film = film_repository.find_by_id(id_tayangan=id_tayangan)
+
+        data_json = {
+            'film': film.to_json()
+        }
+
+        return Response(message='Success get film details!', data=data_json, status=status.HTTP_200_OK)
 
 
 class FilmUlasanView(APIView):
     def post(self, request: Request, id_tayangan: str) -> Response:
-        ...
+        film_repository = FilmRepository()
+        film = film_repository.find_by_id(id_tayangan=id_tayangan)
