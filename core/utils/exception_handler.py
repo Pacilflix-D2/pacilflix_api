7de856a136e5d512fe8a14ddb26d35fa6f3d0520
Arxeno <rmfbkw04@gmail.com>
2024-05-04 19:16:43 +0700

    update exception handler

--- core/utils/exception_handler.py
+++ core/utils/exception_handler.py
@@ -2,10 +2,19 @@ from typing import Any
 from rest_framework.response import Response
 from rest_framework import status
 
+from core.utils.exceptions.not_found import NotFoundException
+
 
 def custom_exception_handler(exc: Exception, context: dict[str, Any]) -> Response:
+    code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    message = 'Internal Server Error'
+
+    if isinstance(exc, NotFoundException):
+        code = status.HTTP_404_NOT_FOUND
+        message = str(exc)
+
     return Response(data={
-        'code': status.HTTP_500_INTERNAL_SERVER_ERROR,
+        'code': code,
         'success': False,
-        'message': 'Internal Server Error'
-    }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        'message': message
+    }, status=code)
