efde4740bd976bbac6cc56588d65dc3e1ce516e3
Arxeno <rmfbkw04@gmail.com>
2024-05-04 19:51:20 +0700

    add more exceptions

--- core/utils/exception_handler.py
+++ core/utils/exception_handler.py
@@ -2,7 +2,10 @@ from typing import Any
 from rest_framework.response import Response
 from rest_framework import status
 
+from core.utils.exceptions.bad_request import BadRequestException
+from core.utils.exceptions.forbidden import ForbiddenException
 from core.utils.exceptions.not_found import NotFoundException
+from core.utils.exceptions.unauthorized import UnauthorizedException
 
 
 def custom_exception_handler(exc: Exception, context: dict[str, Any]) -> Response:
@@ -11,6 +14,15 @@ def custom_exception_handler(exc: Exception, context: dict[str, Any]) -> Respons
 
     if isinstance(exc, NotFoundException):
         code = status.HTTP_404_NOT_FOUND
+        message = str(exc)
+    elif isinstance(exc, ForbiddenException):
+        code = status.HTTP_403_FORBIDDEN
+        message = str(exc)
+    elif isinstance(exc, UnauthorizedException):
+        code = status.HTTP_401_UNAUTHORIZED
+        message = str(exc)
+    elif isinstance(exc, BadRequestException):
+        code = status.HTTP_400_BAD_REQUEST
         message = str(exc)
 
     return Response(data={
--- core/utils/exceptions/not_found.py
+++ core/utils/exceptions/not_found.py
@@ -1,3 +1,2 @@
 class NotFoundException(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
+    pass
