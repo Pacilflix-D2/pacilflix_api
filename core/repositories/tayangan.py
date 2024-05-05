856c8a4f7ff773e3c74a4918852fe38944e07f05
Arxeno <rmfbkw04@gmail.com>
2024-05-05 20:51:28 +0700

    feat: top 10 tayangan (by alphabet)

--- core/repositories/tayangan.py
+++ core/repositories/tayangan.py
@@ -1,5 +1,6 @@
 from core.models.tayangan import Tayangan
 from core.repositories.database import Database
+from core.utils.exceptions.not_found import NotFoundException
 
 
 class TayanganRepository(Database):
@@ -10,7 +11,31 @@ class TayanganRepository(Database):
         ...
 
     def find_top_10_tayangan_global(self) -> list[Tayangan]:
-        ...
+        try:
+            tayangan_tuples = self.query(
+                'SELECT * FROM tayangan ORDER BY judul LIMIT 10')
+        except:
+            raise NotFoundException('Cannot find shows.')
+
+        if len(tayangan_tuples) == 0:
+            raise NotFoundException('Cannot find shows.')
+
+        result: list[Tayangan] = []
+        for tayangan in tayangan_tuples:
+            result.append(
+                Tayangan(
+                    id=str(tayangan[0]),
+                    judul=tayangan[1],
+                    sinopsis=tayangan[2],
+                    asal_negara=tayangan[3],
+                    sinopsis_trailer=tayangan[4],
+                    url_video_trailer=tayangan[5],
+                    release_date_trailer=tayangan[6],
+                    id_sutradara=str(tayangan[7]),
+                )
+            )
+
+        return result
 
     def find_top_tayangan_from_user_country(self, country: str) -> list[Tayangan]:
         ...
--- shows/views.py
+++ shows/views.py
@@ -1,11 +1,21 @@
+from typing import Any
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from core.repositories.tayangan import TayanganRepository
 from core.utils.response import Response
+from rest_framework import status
 
 
 class Top10TayanganView(APIView):
     def get(self, request: Request) -> Response:
-        ...
+        tayangan_repository = TayanganRepository()
+        top_10_shows = tayangan_repository.find_top_10_tayangan_global()
+
+        data_json: list[dict[str, Any]] = []
+        for show in top_10_shows:
+            data_json.append(show.to_json())
+
+        return Response(message='Success get top 10 shows!', data=data_json, status=status.HTTP_200_OK)
 
 
 class SearchTayanganView(APIView):
