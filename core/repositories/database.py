c49782feed43e954806843c9b84a151912fe8024
Arxeno <rmfbkw04@gmail.com>
2024-05-06 16:17:26 +0700

    fix(database): private

--- core/repositories/database.py
+++ core/repositories/database.py
@@ -6,11 +6,11 @@ from django.db import connection
 class Database:
 
     def __init__(self) -> None:
-        self.db_name = environ.get('DB_NAME'),
-        self.db_user = environ.get('DB_USER'),
-        self.db_pass = environ.get('DB_PASS'),
-        self.db_host = environ.get('DB_HOST'),
-        self.db_port = environ.get('DB_PORT')
+        self._db_name = environ.get('DB_NAME'),
+        self._db_user = environ.get('DB_USER'),
+        self._db_pass = environ.get('DB_PASS'),
+        self._db_host = environ.get('DB_HOST'),
+        self._db_port = environ.get('DB_PORT')
 
     # this could possibly throws error, so wrap this method with try catch
     def query(self, query: str) -> list[tuple[Any, ...]]:
