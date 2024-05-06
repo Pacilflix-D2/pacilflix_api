9c0776357006bc94735a1207086a7056053ed05b
Arxeno <rmfbkw04@gmail.com>
2024-05-06 16:44:18 +0700

    fix(auth): remove transport

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.request import Request
-
-from authentication.transports import LoginResponse, RegisterResponse
 from core.utils.response import Response
 
 
 class LoginView(APIView):
-    def post(self, request: Request) -> Response[LoginResponse]:
+    def post(self, request: Request) -> Response:
         ...
 
 
 class RegisterView(APIView):
-    def post(self, request: Request) -> Response[RegisterResponse]:
+    def post(self, request: Request) -> Response:
         ...
