ac7a51787ffdc9ab72f064a03364e3ffe694f534
Arxeno <rmfbkw04@gmail.com>
2024-05-07 10:12:18 +0700

    fix(film): add more details in get detail film

--- film/views.py
+++ film/views.py
@@ -1,9 +1,16 @@
 from typing import Any
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from core.models.contributor import Contributor
 from core.models.film import Film
+from core.models.tayangan import Tayangan
 from core.models.ulasan import Ulasan
+from core.repositories.contributors import ContributorRepository
 from core.repositories.film import FilmRepository
+from core.repositories.gender import GenreRepository
+from core.repositories.pemain import PemainRepository
+from core.repositories.penulis_skenario import PenulisSkenarioRepository
+from core.repositories.tayangan import TayanganRepository
 from core.repositories.ulasan import UlasanRepository
 from core.utils.response import Response
 from rest_framework import status
@@ -31,8 +38,26 @@ class FilmDetailView(APIView):
 
         film = film_repository.find_by_id(id_tayangan=id_tayangan)
 
+        tayangan: Tayangan = TayanganRepository().find_by_id(id=film.id_tayangan)
+        tayangan_json = tayangan.to_json()
+        tayangan_json.pop('id')
+        tayangan_json.pop('id_sutradara')
+
+        genres = GenreRepository().find_by_id_tayangan(id_tayangan=tayangan.id)
+
+        sutradara: Contributor = ContributorRepository().find_by_id(id=tayangan.id_sutradara)
+
+        players = PemainRepository().find_by_id_tayangan(id_tayangan=tayangan.id)
+
+        writers = PenulisSkenarioRepository().find_by_id_tayangan(id_tayangan=tayangan.id)
+
         data_json = {
-            'film': film.to_json()
+            **film.to_json(),
+            **tayangan_json,
+            "sutradara": sutradara.to_json(),
+            "genres": [genre.to_json() for genre in genres],
+            "players": [player.to_json() for player in players],
+            "writers": [writer.to_json() for writer in writers]
         }
 
         return Response(message='Success get film details!', data=data_json, status=status.HTTP_200_OK)
