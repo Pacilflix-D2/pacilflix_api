be9b9149f258da395c5aabdc1fd45b00d2808e1a
Arxeno <rmfbkw04@gmail.com>
2024-05-09 00:30:23 +0700

    feat: top 10 tayangan

--- core/models/tayangan.py
+++ core/models/tayangan.py
@@ -1,5 +1,12 @@
 from datetime import date
+from typing import Literal
 from core.models.base import BaseModel
+from core.models.episode import Episode
+from core.models.riwayat_nonton import RiwayatNonton
+from core.repositories.episode import EpisodeRepository
+from core.repositories.film import FilmRepository
+from core.repositories.riwayat_nonton import RiwayatNontonRepository
+from core.utils.exceptions.internal_server_error import InternalServerException
 
 
 class Tayangan(BaseModel):
@@ -12,3 +19,53 @@ class Tayangan(BaseModel):
         self.url_video_trailer = url_video_trailer
         self.release_date_trailer = release_date_trailer
         self.id_sutradara = id_sutradara
+
+    def get_type(self) -> Literal['FILM', 'SERIES']:
+        try:
+            FilmRepository().find_by_id(id_tayangan=self.id)
+            return 'FILM'
+        except:
+            return 'SERIES'
+
+    def _base_count_total_views(self, watch_histories: list[RiwayatNonton]) -> int:
+        total_views = 0
+        for watch_history in watch_histories:
+            watch_time_in_minutes = (watch_history.end_date_time -
+                                     watch_history.start_date_time).total_seconds() / 60
+            total_duration = self.get_total_duration()
+
+            if watch_time_in_minutes > total_duration:
+                raise InternalServerException(
+                    'Lama menonton tidak dapat melebihi durasi film.')
+
+            watch_time_percentage = (
+                watch_time_in_minutes / total_duration) * 100
+            if watch_time_percentage >= 70:
+                total_views += 1
+
+        return total_views
+
+    def get_total_views(self) -> int:
+        watch_histories: list[RiwayatNonton] = RiwayatNontonRepository(
+        ).find_by_id_tayangan(id_tayangan=self.id)
+
+        return self._base_count_total_views(watch_histories=watch_histories)
+
+    def get_total_views_last_week(self) -> int:
+        watch_histories: list[RiwayatNonton] = RiwayatNontonRepository(
+        ).find_by_id_tayangan_last_week(id_tayangan=self.id)
+
+        return self._base_count_total_views(watch_histories=watch_histories)
+
+    def get_total_duration(self) -> int:
+        if self.get_type() == 'FILM':
+            return FilmRepository().find_by_id(id_tayangan=self.id).durasi_film
+        else:
+            total_duration = 0
+            episodes: list[Episode] = EpisodeRepository(
+            ).find_by_id_series(id_series=self.id)
+
+            for episode in episodes:
+                total_duration += episode.durasi
+
+            return total_duration
--- shows/views.py
+++ shows/views.py
@@ -1,10 +1,7 @@
-from datetime import timedelta
 from typing import Any
 from rest_framework.views import APIView
 from rest_framework.request import Request
-from core.models.riwayat_nonton import RiwayatNonton
 from core.models.tayangan import Tayangan
-from core.repositories.riwayat_nonton import RiwayatNontonRepository
 from core.repositories.tayangan import TayanganRepository
 from core.utils.response import Response
 from rest_framework import status
@@ -13,19 +10,13 @@ from rest_framework import status
 class Top10TayanganView(APIView):
     def get(self, request: Request) -> Response:
         tayangan_repository = TayanganRepository()
-        riwayat_nonton_repository = RiwayatNontonRepository()
+        print('hilih')
 
         shows: list[Tayangan] = tayangan_repository.find_all()
 
         for show in shows:
-            watch_histories: list[RiwayatNonton] = riwayat_nonton_repository.find_by_id_tayangan_last_week(
-                id_tayangan=show.id)
-
-            for watch_history in watch_histories:
-                watch_time: timedelta = watch_history.end_date_time - watch_history.start_date_time
-
-                # lanjut bentar, seharusnya ini ngukur lama nonton dengan durasi full tayangannya, kalau lebih dari 70%, nambah 1 views
-                # if watch_time > timedelta
+            print('hi')
+            print(show.get_total_views_last_week())
 
         data_json: list[dict[str, Any]] = []
         for show in shows:
