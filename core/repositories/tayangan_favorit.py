32ddd0c11ee9e42e040d2698078c5fea4bb7d329
Arxeno <rmfbkw04@gmail.com>
2024-05-09 02:23:20 +0700

    feat: get favorite detail view

--- core/repositories/tayangan_favorit.py
+++ core/repositories/tayangan_favorit.py
@@ -1,11 +1,24 @@
-from datetime import datetime
+from core.models.favorite import Favorite
 from core.models.tayangan_favorit import TayanganFavorit
 from core.repositories.database import Database
+from core.utils.exceptions.not_found import NotFoundException
 
 
 class TayanganFavoritRepository(Database):
     def find_all(self) -> list[TayanganFavorit]:
         ...
 
-    def find_by_username_and_timestamp(self, username: str, timestamp: datetime) -> list[TayanganFavorit]:
-        ...
+    def find_by_daftar_favorit(self, daftar_favorit: Favorite) -> list[TayanganFavorit]:
+        try:
+            tuples = self.select(
+                f"SELECT * FROM TAYANGAN_MEMILIKI_DAFTAR_FAVORIT WHERE username = '{daftar_favorit.username}' AND timestamp = '{daftar_favorit.timestamp}'")
+        except Exception as error:
+            raise NotFoundException(error)
+
+        return [
+            TayanganFavorit(
+                id_tayangan=str(tuple[0]),
+                timestamp=tuple[1],
+                username=tuple[2]
+            ) for tuple in tuples
+        ]
--- favorites/views.py
+++ favorites/views.py
@@ -1,11 +1,80 @@
+from datetime import datetime
+from typing import Any
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from core.models.favorite import Favorite
+from core.models.pengguna import Pengguna
+from core.models.tayangan_favorit import TayanganFavorit
+from core.repositories.favorites import FavoriteRepository
+from core.repositories.tayangan import TayanganRepository
+from core.repositories.tayangan_favorit import TayanganFavoritRepository
+from core.utils.exceptions.bad_request import BadRequestException
+from core.utils.exceptions.unauthorized import UnauthorizedException
+from core.utils.get_user import get_user
 from core.utils.response import Response
+from rest_framework import status
 
 
 class FavoriteListView(APIView):
     def get(self, request: Request) -> Response:
+        user: Pengguna | None = get_user(request=request)
+
+        if not user:
+            raise UnauthorizedException('Must login to access favorite.')
+
+        daftar_favorit_user: list[Favorite] = FavoriteRepository(
+        ).find_by_username(username=user.username)
+
+        data_json = [daftar_favorit.to_json()
+                     for daftar_favorit in daftar_favorit_user]
+
+        return Response(message='Success get daftar favorit!', data=data_json, status=status.HTTP_200_OK)
+
+    def delete(self, request: Request) -> Response:
         ...
+
+
+class FavoriteListDetailView(APIView):
+    def get(self, request: Request) -> Response:
+        tayangan_repository = TayanganRepository()
+
+        user: Pengguna | None = get_user(request=request)
+
+        if not user:
+            raise UnauthorizedException('Must login to access favorite.')
+
+        timestamp: str | None = request.data.get('timestamp', None)
+        if not timestamp:
+            raise BadRequestException('Timetamp is required.')
+        datetime_timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+
+        daftar_favorit_user: Favorite = FavoriteRepository().find_one(
+            timestamp=datetime_timestamp, username=user.username)
+
+        favorit_shows: list[TayanganFavorit] = TayanganFavoritRepository(
+        ).find_by_daftar_favorit(daftar_favorit=daftar_favorit_user)
+
+        favorites_json_list: list[dict[str, Any]] = []
+        for favorit_show in favorit_shows:
+            favorit_show_json = favorit_show.to_json()
+            favorit_show_json.pop('id_tayangan')
+            favorit_show_json.pop('username')
+
+            show = tayangan_repository.find_by_id(id=favorit_show.id_tayangan)
+            show_json = show.to_json()
+
+            favorites_json_list.append({
+                **favorit_show_json,
+                'id': show_json['id'],
+                'judul': show_json['judul']
+            })
+
+        data_json = {
+            'judul': daftar_favorit_user.judul,
+            'favorites': favorites_json_list
+        }
+
+        return Response(message='Success get daftar favorit', data=data_json, status=status.HTTP_200_OK)
 
     def delete(self, request: Request) -> Response:
         ...
